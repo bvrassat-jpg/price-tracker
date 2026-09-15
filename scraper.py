9f4074ba110ebfce1b04e5c840504014645de759
bvrassat-jpg <bvrassat@gmail.com>
2026-09-15 13:34:52 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -68,7 +68,7 @@ HEADERS_BROWSER = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
               "image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "fr-FR,fr;q=0.9,en;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "sec-ch-ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": '"Windows"',
